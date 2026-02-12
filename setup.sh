261fb5ea2247d33d8bb1b73d6890091b9113a948
Anurag R Simha <anurag.rsimha@gmail.com>
2026-02-12 12:26:28 -0500

    Readme update and comment changes.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 echo "--- Project Setup Wizard ---"
 
+echo "Checking for npm"
 while ! command -v npm &> /dev/null; do
     echo "--------------------------------------------------------"
     echo "ERROR: npm (Node Package Manager) was not found."
@@ -17,15 +18,33 @@ while ! command -v npm &> /dev/null; do
         sleep 1 
     fi
 done
-
 echo "npm found! Version: $(npm -v)"
+echo ""
+echo "Checking for git"
+while ! command -v git &> /dev/null; do
+    echo "--------------------------------------------------------"
+    echo "ERROR: Git was not found."
+    echo "1. Please go to https://git-scm.com/downloads"
+    echo "2. Download and install Git for your system."
+    echo "--------------------------------------------------------"
+    
+    read -p "Once you have finished installing Git, type 'Done': " user_input
+    
+    if [[ "$user_input" == "Done" || "$user_input" == "done" ]]; then
+        echo "Re-checking for Git..."
+        sleep 1 
+    fi
+done
 
-echo "Cloning into repository"
+echo "Git found! Version: $(git --version)"
+echo ""
+echo "Cloning into repository..."
 git clone git@gitlab.com:anurag.rsimha/cs530-assignment-3.git
 
 echo "Switching directory"
 cd cs530-assignment-3
 
+echo ""
 echo "Running 'npm install'..."
 npm install
 
